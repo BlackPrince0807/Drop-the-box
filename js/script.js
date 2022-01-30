3079dd27d83137187271522cb58eccc4fbb4734d
Avazbek <foziljonovavazbek0807@gmail.com>
2022-01-30 23:14:26 +0500

    half commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const game__area = document.querySelector('.game__area');
+// let arrw = [0, 20, 40, 60, 100]
+
+
+
+// const boxCreate = () => {
+//     randomElement = arrw[Math.floor(Math.random() * arrw.length)];
+//     console.log(randomElement);
+//     const box = document.createElement('img')
+//     box.className = 'game_box'
+//     box.src = '/image/box.png'
+//     game__area.append(box)
+//     console.log('hello')
+//     box.style.left = `${randomElement}%`
+//     setTimeout(() => {
+//         box.classList.add('transition')
+
+//     }, 1000)
+// }
+// setInterval(boxCreate, 500)
